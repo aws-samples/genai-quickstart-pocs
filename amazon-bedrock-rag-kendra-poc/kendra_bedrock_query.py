b795558754797f2dc3e0e3ba1b4921ecd07d7d4a
Ryan Doty <rdoty@amazon.com>
2024-04-01 15:06:02 -0400

    updated to use Claude3

--- amazon-bedrock-rag-kendra-poc/kendra_bedrock_query.py
+++ amazon-bedrock-rag-kendra-poc/kendra_bedrock_query.py
@@ -7,6 +7,8 @@ import json
 load_dotenv()
 # setting default session with AWS CLI Profile
 boto3.setup_default_session(profile_name=os.getenv('profile_name'))
+# Setup Bedrock client
+bedrock = boto3.client('bedrock-runtime', 'us-east-1', endpoint_url='https://bedrock-runtime.us-east-1.amazonaws.com')
 
 
 def kendraSearch(question):
@@ -39,12 +41,6 @@ def invokeLLM(question, kendra_response):
     :return: Returns the final answer that will be provided to the end-user of the application who asked the original
     question.
     """
-    # Setup Bedrock client
-    bedrock = boto3.client('bedrock-runtime', 'us-east-1', endpoint_url='https://bedrock-runtime.us-east-1.amazonaws.com')
-    # configure model specifics such as specific model
-    modelId = 'anthropic.claude-v2'
-    accept = 'application/json'
-    contentType = 'application/json'
     # prompt that is passed into the LLM with the Kendra Retrieval context and question
     # TODO: FEEL FREE TO EDIT THIS PROMPT TO CATER TO YOUR USE CASE
     prompt_data = f"""\n\nHuman:    
@@ -66,21 +62,30 @@ Context: {kendra_response}
 """
     # body of data with parameters that is passed into the bedrock invoke model request
     # TODO: TUNE THESE PARAMETERS AS YOU SEE FIT
-    body = json.dumps({"prompt": prompt_data,
-                       "max_tokens_to_sample": 8191,
-                       "temperature": 0,
-                       "top_k": 250,
-                       "top_p": 0.5,
-                       "stop_sequences": []
-                       })
-    # Invoking the bedrock model with your specifications
-    response = bedrock.invoke_model(body=body,
-                                    modelId=modelId,
-                                    accept=accept,
-                                    contentType=contentType)
-    # the body of the response that was generated
+    prompt = {
+        "anthropic_version": "bedrock-2023-05-31",
+        "max_tokens": 4096,
+        "temperature": 0.5,
+        "messages": [
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "text",
+                        "text": prompt_data
+                    }
+                ]
+            }
+        ]
+    }
+    # formatting the prompt as a json string
+    json_prompt = json.dumps(prompt)
+    # invoking Claude3, passing in our prompt
+    response = bedrock.invoke_model(body=json_prompt, modelId="anthropic.claude-3-sonnet-20240229-v1:0",
+                                    accept="application/json", contentType="application/json")
+    # getting the response from Claude3 and parsing it to return to the end user
     response_body = json.loads(response.get('body').read())
-    # retrieving the specific completion field, where you answer will be
-    answer = response_body.get('completion')
-    # returning the answer as a final result, which ultimately gets returned to the end user
+    # the final string returned to the end user
+    answer = response_body['content'][0]['text']
+    # returning the final string to the end user
     return answer
