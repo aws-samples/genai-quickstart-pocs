aad6541c4bc947309b5c5b7b64ee7197e330ba11
Sudeesh Sasidharan <148075955+3sudeesh10@users.noreply.github.com>
2025-08-17 17:05:25 -0400

    Delete industry-specific-pocs/energy/agents4energy directory
    test

--- industry-specific-pocs/energy/agents4energy/src/utils/date-utils.ts
+++
@@ -1,6 +0,0 @@
-import { format, parseISO } from 'date-fns';
-
-export const formatDate = (dateString: string): string => {
-    const date = parseISO(dateString);
-    return format(date, 'MMM d, yyyy HH:mm:ss');
-  };
